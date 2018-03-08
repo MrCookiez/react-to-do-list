b862eced697cc5569064b1ea57906a89a973328a
Theodoros Vragkos <bigt3ddybear@yahoo.gr>
2018-03-08 17:57:13 +0200

    Create config.jsx

---
+++ src/config/config.jsx
@@ -0,0 +1,9 @@
+
+	export const config = {
+    apiKey: "Your API KEY",
+    authDomain: "react-notes-c4f1a.firebaseapp.com",
+    databaseURL: "https://react-notes-c4f1a.firebaseio.com",
+    projectId: "react-notes-c4f1a",
+    storageBucket: "react-notes-c4f1a.appspot.com",
+    messagingSenderId: "YOUR SENDER ID"
+  };
