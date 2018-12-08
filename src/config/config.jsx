da737fc84d7c9c56c0bdb6f82a0e9f947fb0c63a
Theo <bigt3ddybear@yahoo.gr>
2018-12-08 10:38:59 +0100

    Update config.jsx

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,9 +1,9 @@
 
 export const DB_CONFIG = {
     apiKey: "Your API KEY",
-    authDomain: "react-notes-c4f1a.firebaseapp.com",
-    databaseURL: "https://react-notes-c4f1a.firebaseio.com",
-    projectId: "react-notes-c4f1a",
-    storageBucket: "react-notes-c4f1a.appspot.com",
+    authDomain: "DOMAIN-NAME.firebaseapp.com",
+    databaseURL: "https://YOUR_NOTES_URL.firebaseio.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "YOUR_NAME.appspot.com",
     messagingSenderId: "YOUR SENDER ID"
   };
